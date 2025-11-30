5717b25b8dd520f474a76cb1c9e389bf0411cd41
Saulo.Figueiredo <saulojcf@gmail.com>
2025-11-30 11:20:38 -0300

    Refatoração utilizando padrão sut e reaproveitamento de código

--- src/adapters/controllers/task/addTask.spec.ts
+++ src/adapters/controllers/task/addTask.spec.ts
@@ -1,6 +1,6 @@
 import { Task } from "../../../entities/task";
 import { AddTask, AddTaskModel } from "../../../usecases";
-import { Validation } from "../../interfaces";
+import { HttpRequest, Validation } from "../../interfaces";
 import { AddTaskController } from "./addTask";
 
 const BODY = {
@@ -9,39 +9,65 @@ const BODY = {
   date: "30/06/2024",
 };
 
-class AddTaskStub implements AddTask {
-  async add(task: AddTaskModel): Promise<Task> {
-    return Promise.resolve({
-      id: "any",
-      ...BODY
-    });
+const makeAddTaskStub = (): AddTask => {
+  class AddTaskStub implements AddTask {
+    async add(task: AddTaskModel): Promise<Task> {
+      return Promise.resolve({
+        id: "any",
+        ...BODY
+      });
+    }
   }
+  return new AddTaskStub();
 }
 
-class ValidationStub implements Validation {
-  validate(data: any): Error | void {
-    return;
+const makeValidationStub = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(data: any): Error | void {
+      return;
+    }
   }
+  return new ValidationStub();
+}
+
+interface SubTypes {
+  addTaskStub: AddTask,
+  validationStub: Validation,
+  sut: AddTaskController
+}
+
+const makeFakeRequest = (): HttpRequest => {
+  return {
+    body: {
+      ...BODY
+    },
+  };
+}
+
+const makeSut = (): SubTypes => {
+  const addTaskStub = makeAddTaskStub();
+  const validationStub = makeValidationStub();
+  const sut = new AddTaskController(
+    addTaskStub,
+    validationStub
+  );
+
+  return {
+    addTaskStub,
+    validationStub,
+    sut
+  };
 }
 
 describe("AddTask Controller", () => {
   test("Deve chamar AddTask com valores corretos", async () => {
-    const httpRequest = {
-      body: {
-        ...BODY
-      },
-    };
+    const httpRequest = makeFakeRequest();
 
-    const addTaskStub = new AddTaskStub();
-
-    const addTaskController = new AddTaskController(
-      addTaskStub,
-      new ValidationStub()
-    );
+    const { addTaskStub, sut } = makeSut();
 
     const addTaskSpy = jest.spyOn(addTaskStub, "add");
 
-    await addTaskController.handle(httpRequest);
+    await sut.handle(httpRequest);
 
     expect(addTaskSpy).toHaveBeenCalledWith(BODY);
   });
